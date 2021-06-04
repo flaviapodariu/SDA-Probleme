36a6b7dfd81c83ba309b67dd5a127f2eb3f8cf0a
flaviapodariu <79100216+flaviapodariu@users.noreply.github.com>
2021-06-04 16:15:09 +0300

    codul bun

--- Airbnb-Aint-RMQ/Arbori de intervale/main.cpp
+++ Airbnb-Aint-RMQ/Arbori de intervale/main.cpp
@@ -4,30 +4,73 @@ using namespace std;
 ifstream fin("arbint.in");
 ofstream fout("arbint.out");
 #define NMAX 100001
-
-long n, m, input[NMAX], log[NMAX], rmq[NMAX][NMAX];
-
+ 
+int n, m, input[NMAX], segTree[NMAX<<2];
+ 
+void construct(int low, int high, int pos)
+{
+    if(low == high)
+    {
+        segTree[pos] = input[low];
+        return;
+    }
+    int mid = (low+high)/2;
+    construct(low, mid, 2*pos);
+    construct(mid+1, high, 2*pos+1);
+    segTree[pos] = max(segTree[2*pos], segTree[2*pos+1]);
+}
+ 
+ 
+int maxim(int qlow, int qhigh, int low, int high, int pos)
+{
+ 
+    if(qlow <= low && qhigh>= high)  //intervalul cerut include intervalul actual
+        return segTree[pos];
+ 
+    if(qlow > high || qhigh < low)   //intervalele nu se suprapun
+        return -1;
+ 
+    int mid = (low +high)/2;
+    return max(maxim(qlow, qhigh, low, mid, pos*2),
+                maxim(qlow, qhigh, mid+1, high, pos*2+1));
+}
+void update(int low, int high, int pos, int a, int b)
+{
+     if(low == high)
+     {
+         segTree[pos] = b;
+         input[a] = b;
+         return;
+     }
+     int mid = (low+high)/2;
+     if(a <= mid)
+        update(low, mid, pos*2,a, b);
+     else
+        update(mid+1, high, pos*2+1,a, b);
+     segTree[pos] = max(segTree[pos*2], segTree[pos*2+1]);
+ 
+}
 int main()
 {
- fin >> n >> m;
- for(long i = 1; i <= n; ++i)
-     fin >> a[i];
- log[1] = 0;
- for(long i = 2;i <=n; ++i)
-     log[i] = log[i/2]+1;
- for(long i =1; 2*i <=n; i++)
-    for(long j = 1; j <= n- 2*i+1; ++j)
+    int op, a, b;
+    fin >> n >> m;
+    for(int i = 1; i <= n; ++i)
+        fin >> input[i];
+    construct(1,n,1);
+ 
+    for(int i = 0; i < m; ++i)
     {
-     long l = 1 << (i-1);
-     rmq[i][j] = min(rmq[i-1][j], rmq[i-1][j+1]);
+        fin >> op >> a >> b;
+        switch(op)
+        {
+        case 0:
+            fout << maxim(a, b,1,n,1)<<"\n";
+            break;
+        case 1:
+            update(1,n,1,a,b);
+            break;
+        }
     }
-  for(long i = 1; i <= ; ++i)
-  {
-     fin >> x >>y;
-     diferenta = y-x+1;
-     long l = log[diferenta];
-
-  }
-
+ 
     return 0;
 }
